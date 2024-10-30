c95306bb5ef588c9a9a5b4b7cbb34362fdc89e62
Marcos Umpierre <45219316+unpierrez@users.noreply.github.com>
2024-10-29 23:06:34 -0300

    #oculos-e-perfumes

--- main.js
+++ main.js
@@ -8,21 +8,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const categoriesSlider = document.querySelector('.categories-slider');
     const recommendedSlider = document.querySelector('.recommended-slider');
+    const glassesSlider = document.querySelector('.glasses-slider');
+    const perfumeSlider = document.querySelector('.perfume-slider');
 
-    const slideLeft = () => {
-        categoriesSlider.scrollBy({ left: -300, behavior: 'smooth' });
-        recommendedSlider.scrollBy({ left: -300, behavior: 'smooth' });
+    const slideLeft = (slider) => {
+        slider.scrollBy({ left: -300, behavior: 'smooth' });
     }
 
-    const slideRight = () => {
-        categoriesSlider.scrollBy({ left: 300, behavior: 'smooth' });
-        recommendedSlider.scrollBy({ left: 300, behavior: 'smooth' });
+    const slideRight = (slider) => {
+        slider.scrollBy({ left: 300, behavior: 'smooth' });
     }
 
-    document.querySelector('.slider-nav.left').addEventListener('click', slideLeft);
-    document.querySelector('.slider-nav.right').addEventListener('click', slideRight);
-    document.querySelector('.recommended-nav.left').addEventListener('click', slideLeft);
-    document.querySelector('.recommended-nav.right').addEventListener('click', slideRight);
+    document.querySelector('.slider-nav.left').addEventListener('click', () => slideLeft(categoriesSlider));
+    document.querySelector('.slider-nav.right').addEventListener('click', () => slideRight(categoriesSlider));
+
+    document.querySelector('.recommended-nav.left').addEventListener('click', () => slideLeft(recommendedSlider));
+    document.querySelector('.recommended-nav.right').addEventListener('click', () => slideRight(recommendedSlider));
+
+    document.querySelector('.glasses-nav.left').addEventListener('click', () => slideLeft(glassesSlider));
+    document.querySelector('.glasses-nav.right').addEventListener('click', () => slideRight(glassesSlider));
+
+    document.querySelector('.perfume-nav.left').addEventListener('click', () => slideLeft(perfumeSlider));
+    document.querySelector('.perfume-nav.right').addEventListener('click', () => slideRight(perfumeSlider));
 
     const recommendedAutoSlide = () => {
         const scrollAmount = 460;
